52eaac862d9408efd4faafd9430cda8e23c056dd
bronzegitter <user1@MyComputer>
2018-12-28 12:49:04 +0900

    フォワード、リダイレクト実装

--- PracticeSpringBoot/src/main/java/com/practice/java/HelloController.java
+++ PracticeSpringBoot/src/main/java/com/practice/java/HelloController.java
@@ -2,12 +2,25 @@ package com.practice.java;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
 	@RequestMapping("/")
-	public String index() {
+	public ModelAndView index(ModelAndView mav) {
 
-		return "index";
+		mav.setViewName("index");
+
+		return mav;
+	}
+
+	@RequestMapping("/other")
+	public String Other() {
+		return "redirect:/";
+	}
+
+	@RequestMapping("/home")
+	public String home() {
+		return "forward:/";
 	}
 }
