ef44cb7f8ad89d46984419303f2587731fa63416
bronzegitter <user1@MyComputer>
2019-01-04 12:35:09 +0900

    配列を使用して表を作成

--- PracticeSpringBoot/src/main/java/com/practice/java/HelloController.java
+++ PracticeSpringBoot/src/main/java/com/practice/java/HelloController.java
@@ -1,26 +1,23 @@
 package com.practice.java;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-@Controller
+import com.practice.java.dto.ControllerDto;
+
+
+
+@RestController
 public class HelloController {
+
 	@RequestMapping("/")
 	public ModelAndView index(ModelAndView mav) {
 
 		mav.setViewName("index");
-
+		mav.addObject("msg", "current data.");
+		ControllerDto obj = new ControllerDto(123, "hanako", "hanako@flower");
+		mav.addObject("object",obj);
 		return mav;
-	}
-
-	@RequestMapping("/other")
-	public String Other() {
-		return "redirect:/";
-	}
-
-	@RequestMapping("/home")
-	public String home() {
-		return "forward:/";
 	}
 }
